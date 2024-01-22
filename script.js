da4df0fd72184ab8e849bde3e494262f3d5cbc75
Arlan Santos <arlan05sts@gmail.com>
2024-01-22 12:37:08 -0300

    Implementando JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Meu Script")
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+  function backspace(){
+    const value = document.getElementById('display').value;
+    document.getElementById('display').value = value.slice(0, -1);
+  }
+
+  function calculate() {
+    try {
+      const result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
